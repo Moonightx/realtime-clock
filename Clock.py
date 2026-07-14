1f24de4d386eedc1d72f4a7fae2c660ddc49ee2a
Moonightx <fatahilahnabilfawwaz@gmail.com>
2026-07-14 14:34:57 +0700

    menambahkan hari dan tanggal

--- Clock.py
+++ Clock.py
@@ -8,9 +8,18 @@ root.geometry("480x260")
 time_label = tk.Label(root, text="00:00:00", font=("Consolas", 40))
 time_label.pack(pady=50)
 
+date_label = tk.Label(root, text="", font=("Segoe UI", 14))
+date_label.pack()
+
 def update_time():
-    waktu_sekarang = datetime.now().strftime("%H:%M:%S")
+    now = datetime.now()
+
+    waktu_sekarang = now.strftime("%H:%M:%S")
     time_label.config(text=waktu_sekarang)
+
+    tanggal_sekarang = now.strftime("%A, %d %B %Y")
+    date_label.config(text=tanggal_sekarang)
+
     root.after(1000, update_time)
 
 update_time()
